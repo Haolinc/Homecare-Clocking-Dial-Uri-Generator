bfea53057eb6112039b7eab098b37c9435492ad7
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2025-09-13 18:39:11 -0400

    added tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <p className="text-3xl font-bold underline">Something</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
