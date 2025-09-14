ee8d5d7ffee62943d9a6bfcc55eac86876f100b1
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2025-09-14 15:56:15 -0400

    remove original index.html and service.js, and updated button style

--- frontend/src/components/FunctionalButton.jsx
+++ frontend/src/components/FunctionalButton.jsx
@@ -1,5 +1,5 @@
 export default function FunctionalButton ({text, onClickFunction}) {
-    const className = 'text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
+    const className = 'w-50 h-10 text-black bg-gradient-to-r from-gray-200 via-gray-200 to-gray-200 hover:bg-gradient-to-br font-medium text-lg px-5 py-2.5 text-center me-2 mb-2'
     return (
         <button 
             type="button"
